3a95a50965909209bb1858c4e7575829d91b3254
Wen Junhua <wenjh1998@gmail.com>
2024-05-25 12:38:48 +0800

    refactor: update pages

--- src/Pages/AboutPage.tsx
+++ src/Pages/AboutPage.tsx
@@ -1,14 +1,10 @@
-import React from "react";
+import * as React from "react";
 import styled from "styled-components";
 import ImageSection from "../Components/ImageSection";
 import Title from "../Components/Title";
 import { MainLayout } from "../styles/Layouts";
 import InterestsSection from "../Components/InterestsSection";
 
-/**
- * AboutPage Component is the about page of the website.
- * @returns {React.JSX.Element}
- */
 const AboutPage = () => (
   <MainLayout>
     <AboutStyled>
--- src/Pages/PortfoliosPage.tsx
+++ src/Pages/PortfoliosPage.tsx
@@ -1,13 +1,9 @@
-import React from "react";
+import * as React from "react";
 import { MainLayout, InnerLayout } from "../styles/Layouts";
 import Title from "../Components/Title";
 import portfolios from "../data/portfolios";
 import Menu from "../Components/Menu";
 
-/**
- * PortfoliosPage Component is the portfolios page of the website.
- * @returns {React.JSX.Element}
- */
 const PortfoliosPage = () => (
   <MainLayout>
     <Title title={"Portfolios"} span={"portfolios"} />
