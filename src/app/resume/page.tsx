16080eb3c313cc29d45cd5bedb052b1174db807a
Wen Junhua <wenjh1998@gmail.com>
2025-12-27 21:52:06 +0800

    chore: remove skills section

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,17 +4,15 @@ import Resume from "../../Components/Resume";
 import { MainLayout } from "../../styles/Layouts";
 import { Metadata } from "next";
 
-
 export const metadata: Metadata = {
-  title: 'Resume',
-  description: 'Work, Skills & Education background',
-  manifest: '/manifest.json',
-}
+  title: "Resume",
+  description: "Work, Skills & Education background",
+  manifest: "/manifest.json",
+};
 
 const ResumePage = () => {
   return (
     <MainLayout>
-      <Skills />
       <Resume />
     </MainLayout>
   );
