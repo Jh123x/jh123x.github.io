6d41831b22c587acfe90d094f0a2f8cbea77d64b
Wen Junhua <wenjh1998@gmail.com>
2026-01-02 23:27:51 +0800

    feat: update title to use material ui

--- src/Components/Title.tsx
+++ src/Components/Title.tsx
@@ -1,69 +1,53 @@
 "use client";
 import * as React from "react";
 import * as types from "../data/types";
-import styled from "styled-components";
-import { Typography } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 
-const Title = ({ title, span }: types.Title) => (
-  <TitleStyled>
-    <Typography variant="h2">{title}</Typography>
-  </TitleStyled>
-);
-
-const TitleStyled = styled.div`
-  position: relative;
-  h2 {
-    color: var(--white-color);
-    font-size: 3.1rem;
-    font-weight: 600;
-    text-transform: uppercase;
-    position: relative;
-    padding-bottom: 0.7rem;
-    @media screen and (max-width: 496px) {
-      font-size: 2.8rem;
-    }
-    @media screen and (max-width: 370px) {
-      font-size: 2rem;
-    }
-    &::before {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      width: 7.4rem;
-      height: 0.33rem;
-      background-color: var(--background-light-color-2);
-      border-radius: 15px;
-      left: 0;
-    }
-    &::after {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      width: 3.5rem;
-      height: 0.33rem;
-      background-color: var(--primary-color);
-      border-radius: 15px;
-      left: 0;
-    }
-    span {
-      font-weight: 900;
-      color: rgba(25, 29, 43, 0.44);
-      font-size: 5rem;
-      position: absolute;
-      left: 0;
-      top: 30%;
-      z-index: -1;
-      @media screen and (max-width: 620px) {
-        font-size: 4rem;
-      }
-      @media screen and (max-width: 496px) {
-        font-size: 3rem;
-      }
-      @media screen and (max-width: 370px) {
-        font-size: 2rem;
-      }
-    }
-  }
-`;
+const Title = ({ title }: types.Title) => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        color: "#FFF",
+        pb: "0.7rem",
+        textTransform: "uppercase",
+        mb: "0.7rem",
+      }}
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: 0,
+          width: "7.4rem",
+          height: "0.33rem",
+          backgroundColor: theme.palette.background.paper,
+          borderRadius: "15px",
+          left: "0",
+        }}
+      />
+      <Typography
+        variant="h2"
+        sx={{
+          fontWeight: 600,
+          fontSize: "3.1rem",
+        }}
+      >
+        {title}
+      </Typography>
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: 0,
+          width: "3.5rem",
+          height: "0.33rem",
+          backgroundColor: theme.palette.primary.main,
+          borderRadius: "15px",
+          left: 0,
+        }}
+      />
+    </Box>
+  );
+};
 
 export default Title;
