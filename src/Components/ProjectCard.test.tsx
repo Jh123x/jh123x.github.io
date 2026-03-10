34527341609ccf0eb3865bbaa86082bd88c3099c
Wen Junhua <wenjh1998@gmail.com>
2026-03-10 08:23:45 +0800

    test: update test case

--- src/Components/ProjectCard.test.tsx
+++ src/Components/ProjectCard.test.tsx
@@ -5,7 +5,12 @@ import Icon from "../img/blog.svg";
 import { render } from "@testing-library/react";
 import { ThemeProvider } from "@mui/material";
 import { darkTheme } from "../app/app";
-import portfolios from "../data/portfolios";
+import { type LinkType, IconType } from "../data/types";
+
+
+const testLinks: LinkType[] = [
+  { iconType: IconType.github, link: "https://github.com/test", tooltip: "Test GitHub" },
+];
 
 describe("ProjectCard", () => {
   test("should render correctly", () => {
@@ -15,7 +20,7 @@ describe("ProjectCard", () => {
           Title="Test"
           Description="Test Description"
           ImageSrc={Icon.src}
-          Icons={portfolios[0].links}
+          Icons={testLinks}
           TechStack={["Stack1", "Stack2"]}
         />
       </ThemeProvider>,
